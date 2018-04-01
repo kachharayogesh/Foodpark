bdee1c1d29b3eca93073292b7c2031d60f98c7a6
yogesh_kachhara <kachharayogesh@gmail.com>
2018-04-01 18:17:58 +0530

    menu added.

---
+++ Foodpark/js/tab.js
@@ -0,0 +1,12 @@
+function openmenu(cityName) {
+    var i;
+    var x = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    document.getElementById(cityName).style.display = "block"; 
+}
+                   
+console.log('john');
+
+
